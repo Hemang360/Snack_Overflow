fe184f7fc5736dee8e72034961144226036a4aa1
Shankhanil Saha <shankhanilsaha@gmail.com>
2025-09-11 23:52:15 +0530

    setup firebase and app

---
+++ app/src/main/java/com/snackoverflow/Ayurveda/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.snackoverflow.Ayurveda
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.snackoverflow.Ayurveda.ui.theme.AyurvedaBlockChainTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AyurvedaBlockChainTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AyurvedaBlockChainTheme {
+        Greeting("Android")
+    }
+}
